ae51534a527b949ff7b165a89d0168bf97149792
blackart <blackart.ru@gmail.com>
2017-05-12 19:21:23 +0800

    w

--- tagCloud/index.js
+++ tagCloud/index.js
@@ -10,6 +10,46 @@ anychart.onDocumentReady(function () {
     chart = anychart.tagCloud();
     chart.title('Top 250 word of Alice In Wounderland.');
     chart.data(text, {mode: 'byWord', minLength: 4, maxItems: 250});
+    chart.data([
+      {"x": "poem", "value": 80, 'category': 'apply'},
+      {"x": "peom", "value": 44, 'category': 'orange'},
+      {"x": "moep", "value": 40, 'category': 'cherry'},
+      {"x": "meop", "value": 36, 'category': 'apply'},
+      {"x": "emop", "value": 32, 'category': 'orange'},
+      {"x": "epom", "value": 28, 'category': 'cherry'},
+      {"x": "opem", "value": 24, 'category': 'apply'},
+      {"x": "omep", "value": 20, 'category': 'orange'},
+      {"x": "mope", "value": 56, 'category': 'cherry'},
+      {"x": "mepo", "value": 12},
+      {"x": "pemo", "value": 10},
+      {"x": "pome", "value": 10}
+    ], {mode: 'byWord', minLength: 4, maxItems: 250});
+    chart.colorRange(true);
+    // chart.colorScale(anychart.scales.linearColor(anychart.color.singleHueProgression('#3b5998')));
+    chart.colorScale(anychart.scales.ordinalColor());
+    chart.legend(true);
+
+    chart.hovered({
+      // fontFamily: 'Times New Roman',
+      // fontFamily: 'Comic Sans MS',
+      // fontSize: 80,
+      // fontSize: function() {
+      //   return this.sourceValue * 1.1;
+      // },
+      // fontSize: '110%',
+      fill: 'red'
+    });
+
+    chart.selected({
+      // fill: 'green',
+      // fontWeight: 'bold'
+    });
+
+    chart.normal({
+      // fontSize: '140%'
+      // fontSize: null
+    });
+
     chart.container('container').draw();
   });
 });
