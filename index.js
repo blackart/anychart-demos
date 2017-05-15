01ce647705d88bd9d6e8d7e38a3be6ed7f13c06e
blackart <blackart.ru@gmail.com>
2017-05-15 17:39:21 +0800

    w

--- index.js
+++ index.js
@@ -2,12 +2,32 @@ var chart;
 anychart.onDocumentLoad(function() {
   anychart.licenseKey('test-key-32db1f79-cc9312c4');
 
-  var stage = anychart.graphics.create('container', 300, 300);
-  stage.suspend();
-  stage.credits(false);
+  var stage = anychart.graphics.create('container');
 
-  chart = anychart.column([3, 6, 1]);
+  var rect5 = stage.rect(40, 420, 300, 180).stroke('black');
 
-  chart.container(stage).draw();
-  stage.resume();
+  var ordinalScale = anychart.scales.ordinal();
+  ordinalScale.values([
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ]);
+
+  axis6 = anychart.standalones.axes.linear();
+  axis6.parentBounds(rect5.getBounds())
+      .scale(ordinalScale)
+      .title('staggerLines(4)')
+      .staggerMode(true)
+      .staggerLines(4);
+  axis6.labels().rotation(-10);
+  axis6.container(stage).draw();
 });
