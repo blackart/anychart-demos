fa21b5cc68bb314eaa9efd2e8cf5c0f286ef2691
Sergey Medvedev <blackart.ru@gmail.com>
2017-06-02 02:34:39 +0800

    fix text wrap

--- index-t.js
+++ index-t.js
@@ -1,26 +1,129 @@
 anychart.onDocumentReady(function() {
-  var stage = anychart.graphics.create('container', 1000, 300);
+  var stage = anychart.graphics.create('container');
   stage.suspend();
+
+  var t = 'This is a long and Supercalifragilisticexpialidocious sentence. 次の単語グレートブリテンおよび北アイルランド連合王国で本当に大きな言葉 在大不列顛和北愛爾蘭聯合王國下面的話真的很大的話 다음 단어 그레이트 브리튼 및 북 아일랜드 연합 왕국에 정말 큰 단어';
+  var width = 160;
+  var textOverflow = '...';
+
+  anychart.graphics.fontSize = 16;
+  anychart.graphics.fontFamily = 'Times New Roman';
+
+
+  //normal
 
   var text = stage.text();
   text
-      .x(300)
-      .text('This is a long and Supercalifragilisticexpialidocious sentence. 次の単語グレートブリテンおよび北アイルランド連合王国で本当に大きな言葉')
-      // .text('Если число протоновпротоновпротоновпротоновпротонов в ядре совпадает с числом электронов, то атом в целом оказывается электрически протоновпротоновпротоновпротоновпротонов.')
-      // .text('Flessibilità/Adattabilità al cambiamento')
-      // .text('first chart sdgf isagfo gsaiyf oasdg foiysdagf uhsdaiufg sadi usdi asidfh ipsua   hsdia.')
-      .width(160)
-      // .textIndent(150)
-      // .height(60)
-      .fontFamily('Times New Roman')
-      .fontSize(16)
-      .wordWrap('normal')
-      // .textWrap('byLetter')
-      .textOverflow('...')
-      // .hAlign('center');
-
-  // stage.rect(300, 0, 200, 500);
+      .y(0)
+      .text(t)
+      .width(width)
+      .textOverflow(textOverflow)
+      .wordBreak('normal')
+      .wordWrap('normal');
   stage.rect().setBounds(text.getBounds());
+
+  text = stage.text();
+  text
+      .y(0).x(200)
+      .wordWrap('normal')
+      .text('word-break: normal;\n word-wrap: normal;');
+  stage.rect().setBounds(text.getBounds());
+
+
+  text = stage.text();
+  text
+      .y(230)
+      .text(t)
+      .width(width)
+      .textOverflow(textOverflow)
+      .wordBreak('keep-all')
+      .wordWrap('normal');
+  stage.rect().setBounds(text.getBounds());
+
+  text = stage.text();
+  text
+      .y(230).x(200)
+      .wordWrap('normal')
+      .text('word-break: keep-all;\n word-wrap: normal;');
+  stage.rect().setBounds(text.getBounds());
+
+
+  text = stage.text();
+  text
+      .y(460)
+      .text(t)
+      .width(width)
+      .textOverflow(textOverflow)
+      .wordBreak('break-all')
+      .wordWrap('normal');
+  stage.rect().setBounds(text.getBounds());
+
+  text = stage.text();
+  text
+      .y(460).x(200)
+      .wordWrap('normal')
+      .text('word-break: break-all;\n word-wrap: normal;');
+  stage.rect().setBounds(text.getBounds());
+
+
+  //break-word
+
+  text = stage.text();
+  text
+      .y(0)
+      .x(400)
+      .text(t)
+      .width(width)
+      .textOverflow(textOverflow)
+      .wordBreak('normal')
+      .wordWrap('break-word');
+  stage.rect().setBounds(text.getBounds());
+
+  text = stage.text();
+  text
+      .y(0).x(600)
+      .wordWrap('normal')
+      .text('word-break: normal;\n word-wrap: break-word;');
+  stage.rect().setBounds(text.getBounds());
+
+
+  text = stage.text();
+  text
+      .y(230)
+      .x(400)
+      .text(t)
+      .width(width)
+      .textOverflow(textOverflow)
+      .wordBreak('keep-all')
+      .wordWrap('break-word');
+  stage.rect().setBounds(text.getBounds());
+
+  text = stage.text();
+  text
+      .y(230).x(600)
+      .wordWrap('normal')
+      .text('word-break: keep-all;\n word-wrap: break-word;');
+  stage.rect().setBounds(text.getBounds());
+
+
+  text = stage.text();
+  text
+      .y(460)
+      .x(400)
+      .text(t)
+      .width(width)
+      .textOverflow(textOverflow)
+      .wordBreak('break-all')
+      .wordWrap('break-word');
+  stage.rect().setBounds(text.getBounds());
+
+  text = stage.text();
+  text
+      .y(460).x(600)
+      .wordWrap('normal')
+      .text('word-break: break-all;\n word-wrap: break-word;');
+  stage.rect().setBounds(text.getBounds());
+
 
   stage.resume();
 });
