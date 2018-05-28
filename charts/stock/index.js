96bf836556d0dbe956f6da0bfacc18d0c3c5f3af
blackart <blackart.ru@gmail.com>
2018-05-28 17:44:03 +0800

    temp

--- charts/stock/index.js
+++ charts/stock/index.js
@@ -23,15 +23,16 @@ anychart.onDocumentReady(function() {
 
   // create stock chart
   chart = anychart.stock();
+  // chart.margin().left(100);
 
-  var firstPlot = chart.plot(0);
-  firstPlot.xAxis().background().enabled(true);
+  // var firstPlot = chart.plot(0);
+  // firstPlot.xAxis().background().enabled(true);
   // firstPlot.xGrid().enabled(true);
   // firstPlot.yGrid().enabled(true);
   // firstPlot.xMinorGrid().enabled(true);
   // firstPlot.yMinorGrid().enabled(true);
 
-  var candlestick = firstPlot.candlestick(mapping).name('MSFT');
+  // var candlestick = firstPlot.candlestick(mapping).name('MSFT');
 
   var secondPlot = chart.plot(1);
   secondPlot.height('30%');
@@ -41,6 +42,18 @@ anychart.onDocumentReady(function() {
   // secondPlot.xMinorGrid().enabled(true);
   // secondPlot.yMinorGrid().enabled(true);
   secondPlot.yAxis().labels().format('{%tickValue}{scale:(1)(1000)(1000)(1000)|( d)( th)( M)( B)}');
+
+  secondPlot.priceIndicator(0)
+      .label().format('{%value}{scale:(1)(1000)(1000)(1000)|( d)( th)( M)( B),decimalsCount:0}');
+  secondPlot.priceIndicator(0)
+      .value(1178550000000);
+
+  // secondPlot.priceIndicator(1)
+  //     .label().format('{%value}{scale:(1)(1000)(1000)(1000)|( d)( th)( M)( B),decimalsCount:0}');
+  // secondPlot.priceIndicator(1)
+  //     .value(1172678400000);
+
+  secondPlot.crosshair().yLabel().format('{%value}{scale:(1)(1000)(1000)(1000)|( d)( th)( M)( B),decimalsCount:0}');
 
   var series = secondPlot.line(mapping).name('MSFT');
 
@@ -52,48 +65,49 @@ anychart.onDocumentReady(function() {
   // series.risingHatchFill(candlestick.risingHatchFill());
   // series.fallingHatchFill(candlestick.fallingHatchFill());
 
+  var tLimit = 80000000;
+  var lLimit = 60000000;
   var colorScale = anychart.scales.ordinalColor();
   colorScale.ranges([
     {
-      less: 50000000,
-      color: '2 red'
+      less: lLimit,
+      color: 'red'
     },
     {
-      greater: 50000000,
-      color: '2 blue'
+      from: tLimit,
+      to: lLimit,
+      color: 'green'
+    },
+    {
+      greater: tLimit,
+      color: 'blue'
     }
   ])
 
   series.colorScale(colorScale);
 
   // series.allowPointSettings(true);
-  series.fill(function(arg) {
-    // console.log(this.x, this.value, this.index);
-    // console.log(this.getData('open'), this.getData('close'));
-    var rising = +this.getData('open') < +this.getData('close');
-    return rising ? candlestick.risingFill() : candlestick.fallingFill();
-    // return this.sourceColor;
-  });
+  // series.fill(function(arg) {
+  //   // console.log(this.x, this.value, this.index);
+  //   // console.log(this.getData('open'), this.getData('close'));
+  //   var rising = +this.getData('open') < +this.getData('close');
+  //   return rising ? candlestick.risingFill() : candlestick.fallingFill();
+  //   // return this.sourceColor;
+  // });
 
-  series.stroke(function(arg) {
-    // console.log(this.x, this.value, this.index);
-    // console.log(this.getData('open'), this.getData('close'));
-    var rising = +this.getData('open') < +this.getData('close');
-    return rising ? candlestick.risingFill() : candlestick.fallingFill();
-    // return this.sourceColor;
-  });
-
-  series.hovered().fill(function(arg) {
-    // console.log(this.x, this.value, this.index);
-    // console.log(this.getData('open'), this.getData('close'));
-    var rising = +this.getData('open') < +this.getData('close');
-    return rising ? 'green' : 'red';
-    // return this.sourceColor;
-  });
+  series.stroke('colorscale');
+  //
+  // series.hovered().fill(function(arg) {
+  //   // console.log(this.x, this.value, this.index);
+  //   // console.log(this.getData('open'), this.getData('close'));
+  //   var rising = +this.getData('open') < +this.getData('close');
+  //   return rising ? 'green' : 'red';
+  //   // return this.sourceColor;
+  // });
 
 
   // create scroller series with mapped data
-  chart.scroller().line(scrollerMapping);
+  // chart.scroller().line(scrollerMapping);
 
   // set chart selected date/time range
   chart.selectRange('2007-01-03', '2007-05-20');
