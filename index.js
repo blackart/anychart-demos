fc71a8963b73cfbded3ee2bbd0e590f222802715
blackart <blackart.ru@gmail.com>
2018-06-13 00:08:48 +0800

    fix

--- index.js
+++ index.js
@@ -1,98 +1,28 @@
 anychart.onDocumentReady(function () {
-  // The data used in this sample can be obtained from the CDN
-  // https://cdn.anychart.com/csv-data/msft-daily-short.js
-  // https://cdn.anychart.com/csv-data/orcl-daily-short.js
-  // https://cdn.anychart.com/csv-data/csco-daily-short.js
-  // https://cdn.anychart.com/csv-data/ibm-daily-short.js
 
-  // create data tables on loaded data
-  var msftDataTable = anychart.data.table();
-  msftDataTable.addData(get_msft_daily_short_data());
+  // create data
+  var data = [
+    ["January", 10000],
+    ["February", 12000],
+    ["March", 18000],
+    ["April", 11000],
+    ["May", 9000]
+  ];
 
-  var orclDataTable = anychart.data.table();
-  orclDataTable.addData(get_orcl_daily_short_data());
+  // create a line chart
+  var chart = anychart.line(data);
+  chart.title("Side Position");
 
-  var cscoDataTable = anychart.data.table();
-  cscoDataTable.addData(get_csco_daily_short_data());
+  // change the side position of labels on the y-axis
+  chart.yAxis().labels().position("inside");
+  chart.yAxis().minorLabels().enabled(true);
 
-  var ibmDataTable = anychart.data.table();
-  ibmDataTable.addData(get_ibm_daily_short_data());
+  // change the side position of labels on the x-axis
+  chart.xAxis().labels().position("inside");
 
-  // create stock chart
-  chart = anychart.stock();
-  chart.padding(0);
-  chart.tooltip(false);
+  // set the container id
+  chart.container("container");
 
-  // create first plot on the chart with column series
-  var firstPlot = chart.plot(0);
-  var yScale = firstPlot.yScale();
-  yScale.ticks().count(10);
-
-  var yAxis = firstPlot.yAxis();
-  yAxis.labels().position('inside');
-  yAxis.labels().padding().left(20)
-  yAxis.ticks().enabled(false).position('inside');
-  yAxis.drawFirstLabel(false);
-  yAxis.stroke(null);
-
-  firstPlot.legend().margin().bottom(20);
-
-  var xAxis = firstPlot.xAxis();
-  xAxis.labels().position('inside');
-  xAxis.labels().padding().bottom(20);
-  xAxis.minorLabels().position('inside');
-  xAxis.minorLabels().padding().bottom(20);
-  xAxis.height(0);
-  xAxis.showHelperLabel(false);
-
-  firstPlot.crosshair().xLabel().enabled(false);
-  firstPlot.crosshair().xLabel().anchor('center-bottom');
-  firstPlot.crosshair().xLabel().padding(4, 15, 4, 20);
-  firstPlot.crosshair().xLabel().fontColor('#7c868e');
-  firstPlot.crosshair().xLabel().fontSize(11);
-  firstPlot.crosshair().xLabel().background()
-      .fill(anychart.color.lighten('orange'))
-      .stroke('orange');
-
-
-  firstPlot.crosshair().yStroke(null);
-  firstPlot.crosshair().yLabel().anchor('left-center');
-  firstPlot.crosshair().yLabel().padding(4, 15, 4, 20);
-  firstPlot.crosshair().yLabel().format('{%Value}{decimalsCount:1,zeroFillDecimals:true}');
-  firstPlot.crosshair().yLabel().fontColor('#7c868e');
-  firstPlot.crosshair().yLabel().fontSize(11);
-  firstPlot.crosshair().yLabel().background()
-      .corners(0, 10, 10, 0)
-      .cornerType('cut')
-      .fill(anychart.color.lighten('orange'))
-      .stroke('orange');
-
-
-  indicator = firstPlot.priceIndicator();
-  indicator.value('last-visible');
-  indicator.label().anchor('left-center');
-  indicator.label().padding(4, 15, 4, 20);
-  indicator.label().format('{%Value}{decimalsCount:1,zeroFillDecimals:true}');
-  indicator.label().fontColor('#7c868e');
-  indicator.label().background()
-      .corners(0, 10, 10, 0)
-      .cornerType('cut')
-      .fill((anychart.color.lighten('orange', .9)))
-      .stroke('orange');
-  indicator.stroke(null);
-
-  // create area series on the first plot
-  var msftSeries = firstPlot.area(msftDataTable.mapAs({'value': 4}));
-  msftSeries.name('MSFT');
-  msftSeries.color('orange');
-  msftSeries.fill(function() {
-    return anychart.color.setOpacity(this['sourceColor'], 0.3, true);
-  })
-  msftSeries.hovered().markers().enabled(true);
-
-  chart.scroller(false);
-  // chart.selectRange('2005-01-03', '2005-11-20');
-
-  chart.container('container');
+  // initiate drawing the chart
   chart.draw();
 });
