3bedd4dd2ece5c9c2c93b9dd04a4420a4f8f6942
blackart <blackart.ru@gmail.com>
2018-06-27 17:51:34 +0800

    fixes

--- charts/cartesian/index.js
+++ charts/cartesian/index.js
@@ -43,12 +43,12 @@ colorScale = function(series) {
 
   series.colorScale(colorScale);
 
-  series.stroke(function() {
-    return anychart.color.setThickness(this.scaledColor, 1);
-  });
-  series.fill(function() {
-    return anychart.color.setOpacity(this.scaledColor, .3);
-  });
+  // series.stroke(function() {
+  //   return anychart.color.setThickness(this.scaledColor, 1);
+  // });
+  // series.fill(function() {
+  //   return anychart.color.setOpacity(this.scaledColor, .3);
+  // });
 }
 
 coloringFuncName = 'negativeColoring';
@@ -58,17 +58,17 @@ coloringFunc = this[coloringFuncName];
 configureSeries = function(series) {
   coloringFunc(series);
 
-  // series.highStroke('red');
-  // series.lowStroke('blue');
-  // series.highFill('red .2');
-  // series.lowFill('blue .2');
+  series.highStroke('red');
+  series.lowStroke('blue');
+  series.highFill('red .2');
+  series.lowFill('blue .2');
 
   series.markers(true);
 
-  if (series.stepDirection) {
+  // if (series.stepDirection) {
     // series.stepDirection(anychart.enums.StepDirection.FORWARD);
     // series.stepDirection(anychart.enums.StepDirection.BACKWARD);
-  }
+  // }
 
   // series.hovered()
   //     .negativeStroke('15 red')
@@ -97,11 +97,12 @@ anychart.onDocumentReady(function() {
 
   // map loaded data
   var mapping = dataSet.mapAs({
-    open: 1,
-    high: 2,
-    low: 3,
-    close: 4,
-    value: 5
+    size: 1,
+    open: 2,
+    high: 3,
+    low: 4,
+    close: 5,
+    value: 6
   });
 
   chart = anychart.cartesian();
@@ -119,20 +120,32 @@ anychart.onDocumentReady(function() {
   chart.container('container');
   chart.draw();
 
-  $('#baseline').val(chart.baseline());
-  $('[name=series][series-type=' + series.getType() + ']').attr('checked', 'checked');
-  $('[name=color][value=' + coloringFuncName + ']').attr('checked', 'checked');
-  $('[name=series]').click(function() {
-    chart.container().getStage().suspend();
+
+  var seriesList = $('<select></select>');
+  var seriesTypes = ['area', 'bar', 'box', 'bubble', 'candlestick', 'column', 'jump-line', 'line', 'marker', 'ohlc', 'range-area', 'range-bar', 'range-column', 'range-spline-area', 'range-step-area', 'spline', 'spline-area', 'step-area', 'step-line', 'stick', 'hilo'];
+  for (var i = 0, len = seriesTypes.length; i < len; i++) {
+    var seriesListElement = seriesTypes[i];
+    seriesList.append('<option value="' + seriesListElement + '">' + seriesListElement + '</option>')
+  }
+  $('#series').append(seriesList);
+
+  $(seriesList).change(function(e) {
+
+    var name = $(this).val().replace(/-(.)/g, function(match, p1) {return p1.toUpperCase()});
+    // chart.container().getStage().suspend();
     chart.removeSeriesAt(0);
-    var series = chart[this.value](mapping);
+    var series = chart[name](mapping);
     configureSeries(series);
-    chart.container().getStage().resume();
+    // chart.container().getStage().resume();
   });
+
+  $('#baseline').val(chart.baseline());
+  $(seriesList).val(series.getType());
+  $('[name=color][value=' + coloringFuncName + ']').attr('checked', 'checked');
   $('[name=color]').click(function() {
     chart.container().getStage().suspend();
 
-    var seriesType = $('[name=series]:checked').val();
+    var seriesType = $(seriesList).val().replace(/-(.)/g, function(match, p1) {return p1.toUpperCase()});
     var series;
 
     coloringFunc = window[this.value];
@@ -178,38 +191,38 @@ anychart.onDocumentReady(function() {
 
 function get_msft_daily_short_data() {
   return [
-    ['2004-01-02', 27.58, 27.77, 27.33, 27.45, 444],
-    ['2004-01-03', 27.58, 27.77, 27.33, 27.45, -197],
-    ['2004-01-05', 27.73, 28.18, 27.72, 28.14, -673],
-    ['2004-01-06', 28.19, 28.28, 28.07, 28.24, -469],
-    ['2004-01-07', 28.17, 28.31, 28.01, 28.21, -542],
-    ['2004-01-08', 28.39, 28.48, 28, 28.16, 588],
-    ['2004-01-09', 28.03, 27.06, 27.59, 27.66, 600],
-    ['2004-01-12', 27.67, 26.73, 27.35, 27.57, 558],
-    ['2004-01-13', 27.55, 26.64, 27.26, 27.43, -515],
-    ['2004-01-14', 27.52, 27.73, 27.47, 27.7, -439],
-    ['2004-01-15', 27.55, 27.72, 27.42, 27.54, -585],
-    ['2004-01-16', 27.71, 26.88, 27.53, 27.81, -639],
-    ['2004-01-20', 27.98, 27.2, 27.93, 28.1, 630],
-    ['2004-01-21', 28.13, 27.3, 27.85, 28.3, 535],
-    ['2004-01-22', 28.36, 28.44, 27.94, 28.01, 784],
-    ['2004-01-23', 28.28, 28.76, 28.22, 28.48, -127],
-    ['2004-01-24', 28.28, 28.76, 28.22, 28.48, -127],
-    ['2004-01-25', 28.28, 28.76, 28.22, 28.48, -127],
-    ['2004-01-26', 28.49, 28.83, 28.32, 28.8, -582],
-    ['2004-01-27', 28.64, 27.72, 28.22, 28.25, -631],
-    ['2004-01-28', 28.3, 28.44, 27.47, 27.71, -713],
-    ['2004-01-29', 27.81, 27.95, 27.57, 27.91, -637],
-    ['2004-01-30', 27.84, 27.9, 27.55, 27.65, 405],
-    ['2004-02-02', 27.61, 27.8, 27.24, 27.4, 628],
-    ['2004-02-03', 27.4, 27.55, 27.18, 27.29, -479],
-    ['2004-02-04', 27.22, 27.43, 27.01, 27.01, -606],
-    ['2004-02-05', 27.06, 26.17, 26.83, 26.96, 555],
-    ['2004-02-06', 27.03, 26.19, 26.93, 27.08, 472],
-    ['2004-02-07', 27.03, 26.19, 26.93, 27.08, 472],
-    ['2004-02-08', 27.03, 26.19, 26.93, 27.08, 472],
-    ['2004-02-09', 27.03, 26.19, 26.93, 27.08, 472],
-    ['2004-02-10', 27.03, 27.19, 26.93, 27.08, 472],
-    ['2004-02-11', 27.03, 27.19, 26.93, 27.08, 472]
+    ['2004-01-02', 1, 27.58, 27.77, 27.33, 27.45, 444],
+    ['2004-01-03', 3, 27.58, 27.77, 27.33, 27.45, -197],
+    ['2004-01-05', 5, 27.73, 28.18, 27.72, 28.14, -673],
+    ['2004-01-06', -6, 28.19, 28.28, 28.07, 28.24, -469],
+    ['2004-01-07', -4, 28.17, 28.31, 28.01, 28.21, -542],
+    ['2004-01-08', 5, 28.39, 28.48, 28, 28.16, 588],
+    ['2004-01-09', 3, 28.03, 27.06, 27.59, 27.66, 600],
+    ['2004-01-12', -2, 27.67, 26.73, 27.35, 27.57, 558],
+    ['2004-01-13', 3, 27.55, 26.64, 27.26, 27.43, -515],
+    ['2004-01-14', -7, 27.52, 27.73, 27.47, 27.7, -439],
+    ['2004-01-15', 5, 27.55, 27.72, 27.42, 27.54, -585],
+    ['2004-01-16', 1, 27.71, 26.88, 27.53, 27.81, -639],
+    ['2004-01-20', -3, 27.98, 27.2, 27.93, 28.1, 630],
+    ['2004-01-21', 5, 28.13, 27.3, 27.85, 28.3, 535],
+    ['2004-01-22', 4, 28.36, 28.44, 27.94, 28.01, 784],
+    ['2004-01-23', -3, 28.28, 28.76, 28.22, 28.48, -127],
+    ['2004-01-24', 1, 28.28, 28.76, 28.22, 28.48, -127],
+    ['2004-01-25', 1, 28.28, 28.76, 28.22, 28.48, -127],
+    ['2004-01-26', 3, 28.49, 28.83, 28.32, 28.8, -582],
+    ['2004-01-27', -2, 28.64, 27.72, 28.22, 28.25, -631],
+    ['2004-01-28', 5, 28.3, 28.44, 27.47, 27.71, -713],
+    ['2004-01-29', 3, 27.81, 27.95, 27.57, 27.91, -637],
+    ['2004-01-30', 5, 27.84, 27.9, 27.55, 27.65, 405],
+    ['2004-02-02', -1, 27.61, 27.8, 27.24, 27.4, 628],
+    ['2004-02-03', 2, 27.4, 27.55, 27.18, 27.29, -479],
+    ['2004-02-04', -5, 27.22, 27.43, 27.01, 27.01, -606],
+    ['2004-02-05', -6, 27.06, 26.17, 26.83, 26.96, 555],
+    ['2004-02-06', 4, 27.03, 26.19, 26.93, 27.08, 472],
+    ['2004-02-07', 3, 27.03, 26.19, 26.93, 27.08, 472],
+    ['2004-02-08', -5, 27.03, 26.19, 26.93, 27.08, 472],
+    ['2004-02-09', 1, 27.03, 26.19, 26.93, 27.08, 472],
+    ['2004-02-10', -3, 27.03, 27.19, 26.93, 27.08, 472],
+    ['2004-02-11', 5, 27.03, 27.19, 26.93, 27.08, 472]
   ];
 }
