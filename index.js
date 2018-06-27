4c89c8aa24902cc4fd89b18adeda6ae6f20a744b
blackart <blackart.ru@gmail.com>
2018-06-27 09:43:14 +0800

    w

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
 anychart.onDocumentReady(function () {
+  var data = [];
 
-  // create data
-  var data = [
-    ["January", 10000],
-    ["February", 12000],
-    ["March", 18000],
-    ["April", 11000],
-    ["May", 9000]
-  ];
+  for (var i = 0, len = 100; i < len; i++) {
+    data.push(i);
+  }
 
-  // create a line chart
   var chart = anychart.line(data);
-  chart.title("Side Position");
-
-  // change the side position of labels on the y-axis
-  chart.yAxis().labels().position("inside");
-  chart.yAxis().minorLabels().enabled(true);
-
-  // change the side position of labels on the x-axis
-  chart.xAxis().labels().position("inside");
-
-  // set the container id
-  chart.container("container");
-
-  // initiate drawing the chart
-  chart.draw();
+  chart.xAxis(false).yAxis(false);
+  chart.container("container").draw();
 });
